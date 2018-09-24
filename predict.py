b3f54dbee3c5ba9d355a188f7e47ec82d607c379
Prabhsimran Singh <pskrunner14@gmail.com>
2018-09-25 04:15:06 +0530

    ref train

--- predict.py
+++ predict.py
@@ -4,6 +4,12 @@ import numpy as np
 
 from resnet_utils import convert_to_one_hot
 
+@click.command()
+@click.argument('image_path', type=click.Path(exists=True))
+def main(image_path):
+    resnet50 = keras.models.load_model('models/resnet50.h5')
+    make_prediction(resnet50, image_path)
+
 def make_prediction(model, path=None):
     if path is None:
         raise UserWarning('Image path should not be None!')
@@ -19,12 +25,6 @@ def make_prediction(model, path=None):
     print("Multi-class prediction vector [p(0), p(1), p(2), p(3), p(4), p(5)] : ")
     print(preds)
     print("Model predicts a \"{}\" with {:.2f}% probability".format(np.argmax(preds[0]), preds[0][np.argmax(preds)] * 100))
-
-@click.command()
-@click.argument('image_path', type=click.Path(exists=True))
-def main(image_path):
-    resnet50 = keras.models.load_model('models/resnet50.h5')
-    make_prediction(resnet50, image_path)
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -9,104 +9,53 @@ import keras.backend as K
 K.set_image_data_format('channels_last')
 K.set_learning_phase(1)
 
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+def main():
+    X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 
-# Normalize image vectors
-X_train = X_train_orig / 255.
-X_test = X_test_orig / 255.
+    # Normalize image vectors
+    X_train = X_train_orig / 255.
+    X_test = X_test_orig / 255.
 
-# Convert training and test labels to one hot matrices
-Y_train = convert_to_one_hot(Y_train_orig, 6).T
-Y_test = convert_to_one_hot(Y_test_orig, 6).T
+    # Convert training and test labels to one hot matrices
+    Y_train = convert_to_one_hot(Y_train_orig, 6).T
+    Y_test = convert_to_one_hot(Y_test_orig, 6).T
 
-X_val = X_train[-100: ]
-Y_val = Y_train[-100: ]
+    X_val = X_train[-100: ]
+    Y_val = Y_train[-100: ]
 
-X_train = X_train[: -100]
-Y_train = Y_train[: -100]
+    X_train = X_train[: -100]
+    Y_train = Y_train[: -100]
 
-print ("number of training examples = " + str(X_train.shape[0]))
-print ("number of validation examples = " + str(X_val.shape[0]))
-print ("number of test examples = " + str(X_test.shape[0]))
-print ("X_train shape: " + str(X_train.shape))
-print ("Y_train shape: " + str(Y_train.shape))
-print ("X_val shape: " + str(X_val.shape))
-print ("Y_val shape: " + str(Y_val.shape))
-print ("X_test shape: " + str(X_test.shape))
-print ("Y_test shape: " + str(Y_test.shape))
+    print ("number of training examples = " + str(X_train.shape[0]))
+    print ("number of validation examples = " + str(X_val.shape[0]))
+    print ("number of test examples = " + str(X_test.shape[0]))
+    print ("X_train shape: " + str(X_train.shape))
+    print ("Y_train shape: " + str(Y_train.shape))
+    print ("X_val shape: " + str(X_val.shape))
+    print ("Y_val shape: " + str(Y_val.shape))
+    print ("X_test shape: " + str(X_test.shape))
+    print ("Y_test shape: " + str(Y_test.shape))
 
-def identity_block(X, f, filters, stage, block):
-    # defining name basis
-    conv_name_base = 'res' + str(stage) + block + '_branch'
-    bn_name_base = 'bn' + str(stage) + block + '_branch'
-
-    # Retrieve Filters
-    F1, F2, F3 = filters
+    if os.path.exists('models/resnet50.h5'):
+        resnet50 = keras.models.load_model('models/resnet50.h5')
+    else:
+        resnet50 = create_model(input_shape=(64, 64, 3), classes=6)
+        resnet50.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     
-    # Save the input value. You'll need this later to add back to the main path. 
-    X_shortcut = X
-    
-    # First component of main path
-    X = keras.layers.Conv2D(filters=F1, kernel_size=(1, 1), strides=(1,1), padding='valid', name=conv_name_base + '2a', 
-                            kernel_initializer=keras.initializers.glorot_uniform(seed=0))(X)
-    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
-    X = keras.layers.Activation('relu')(X)
-    
-    # Second component of main path
-    X = keras.layers.Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', 
-                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
-    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
-    X = keras.layers.Activation('relu')(X)
+    resnet50.summary()
+    resnet50.fit(x=X_train, y=Y_train, validation_data=(X_val, Y_val), shuffle=True, epochs=5, batch_size=32, verbose=1)
 
-    # Third component of main path
-    X = keras.layers.Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', 
-                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
-    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+    resnet50.save('models/resnet50.h5')
+    preds = resnet50.evaluate(x=X_test, y=Y_test, verbose=1)
 
-    # Add shortcut value to main path, and pass it through a RELU activation
-    X = keras.layers.Add()([X, X_shortcut])
-    X = keras.layers.Activation('relu')(X)
-    
-    return X
+    print ("Test Loss: {}".format(preds[0]))
+    print ("Test Accuracy: {}".format(preds[1]))
 
-def convolutional_block(X, f, filters, stage, block, s = 2):
-    # defining name basis
-    conv_name_base = 'res' + str(stage) + block + '_branch'
-    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    keras.utils.plot_model(resnet50, to_file='models/resnet50.png')
 
-    # Retrieve Filters
-    F1, F2, F3 = filters
-    
-    # Save the input value
-    X_shortcut = X
-
-    # First component of main path 
-    X = keras.layers.Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', padding = 'valid', 
-                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
-    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
-    X = keras.layers.Activation('relu')(X)
-    
-    # Second component of main path
-    X = keras.layers.Conv2D(F2, (f, f), strides = (1, 1), name = conv_name_base + '2b', padding = 'same', 
-                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
-    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
-    X = keras.layers.Activation('relu')(X)
-
-    # Third component of main path
-    X = keras.layers.Conv2D(F3, (1, 1), strides = (1, 1), name = conv_name_base + '2c', padding = 'valid', 
-                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
-    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
-
-    X_shortcut = keras.layers.Conv2D(F3, (1, 1), strides = (s,s), name = conv_name_base + '1', padding = 'valid', 
-                                    kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X_shortcut)
-    X_shortcut = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
-
-    # Add shortcut value to main path, and pass it through a RELU activation
-    X = keras.layers.Add()([X, X_shortcut])
-    X = keras.layers.Activation('relu')(X)
-    
-    return X
-
+"""
+Creates Residual Network with 50 layers
+"""
 def create_model(input_shape=(64, 64, 3), classes=6):
     # Define the input as a tensor with shape input_shape
     X_input = keras.layers.Input(input_shape)
@@ -158,23 +107,83 @@ def create_model(input_shape=(64, 64, 3), classes=6):
 
     return model
 
-def main():
-    if os.path.exists('models/resnet50.h5'):
-        resnet50 = keras.models.load_model('models/resnet50.h5')
-    else:
-        resnet50 = create_model(input_shape=(64, 64, 3), classes=6)
-        resnet50.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+"""
+Identity Block of ResNet
+"""
+def identity_block(X, f, filters, stage, block):
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
     
-    resnet50.summary()
-    resnet50.fit(x=X_train, y=Y_train, validation_data=(X_val, Y_val), shuffle=True, epochs=5, batch_size=32, verbose=1)
+    # Save the input value. You'll need this later to add back to the main path. 
+    X_shortcut = X
+    
+    # First component of main path
+    X = keras.layers.Conv2D(filters=F1, kernel_size=(1, 1), strides=(1,1), padding='valid', name=conv_name_base + '2a', 
+                            kernel_initializer=keras.initializers.glorot_uniform(seed=0))(X)
+    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = keras.layers.Activation('relu')(X)
+    
+    # Second component of main path
+    X = keras.layers.Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', 
+                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
+    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = keras.layers.Activation('relu')(X)
 
-    resnet50.save('models/resnet50.h5')
-    preds = resnet50.evaluate(x=X_test, y=Y_test, verbose=1)
+    # Third component of main path
+    X = keras.layers.Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', 
+                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
+    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
 
-    print ("Test Loss: {}".format(preds[0]))
-    print ("Test Accuracy: {}".format(preds[1]))
+    # Add shortcut value to main path, and pass it through a RELU activation
+    X = keras.layers.Add()([X, X_shortcut])
+    X = keras.layers.Activation('relu')(X)
+    
+    return X
 
-    keras.utils.plot_model(resnet50, to_file='models/resnet50.png')
+"""
+Convolutional Block of ResNet
+"""
+def convolutional_block(X, f, filters, stage, block, s = 2):
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value
+    X_shortcut = X
+
+    # First component of main path 
+    X = keras.layers.Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', padding = 'valid', 
+                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
+    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = keras.layers.Activation('relu')(X)
+    
+    # Second component of main path
+    X = keras.layers.Conv2D(F2, (f, f), strides = (1, 1), name = conv_name_base + '2b', padding = 'same', 
+                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
+    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = keras.layers.Activation('relu')(X)
+
+    # Third component of main path
+    X = keras.layers.Conv2D(F3, (1, 1), strides = (1, 1), name = conv_name_base + '2c', padding = 'valid', 
+                            kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X)
+    X = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    X_shortcut = keras.layers.Conv2D(F3, (1, 1), strides = (s,s), name = conv_name_base + '1', padding = 'valid', 
+                                    kernel_initializer = keras.initializers.glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+    # Add shortcut value to main path, and pass it through a RELU activation
+    X = keras.layers.Add()([X, X_shortcut])
+    X = keras.layers.Activation('relu')(X)
+    
+    return X
 
 if __name__ == '__main__':
     main()
